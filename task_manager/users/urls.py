f31a090f1c705c2d000725e3415b66f895b02974
dmitriy-ga <Dmitriy2243@yandex.ru>
2023-03-23 14:42:04 +0500

    Fix: E501 and F401

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -4,7 +4,12 @@ from task_manager.users import views
 
 urlpatterns = [
     path('', views.UsersIndex.as_view(), name='users_index'),
+
     path('create/', views.UsersCreate.as_view(), name='users_create'),
-    path('<int:user_id>/update/', views.UsersUpdate.as_view(), name='users_update'),
-    path('<int:user_id>/delete/', views.UsersDelete.as_view(), name='users_delete'),
+
+    path('<int:user_id>/update/',
+         views.UsersUpdate.as_view(), name='users_update'),
+
+    path('<int:user_id>/delete/',
+         views.UsersDelete.as_view(), name='users_delete'),
 ]
