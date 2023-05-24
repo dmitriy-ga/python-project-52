edd83de0263a88ffbb3873726778a65a1aa89349
dmitriy-ga <Dmitriy2243@yandex.ru>
2023-05-24 17:06:34 +0500

    Add: flash message for login

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render
 from task_manager.users.forms import LoginForm
+from django.contrib.messages.views import SuccessMessageMixin
+from django.utils.translation import gettext as _
 
 
 def index(request):
     return render(request, 'index.html')
 
 
-class ViewForLogin(LoginView):
+class ViewForLogin(SuccessMessageMixin, LoginView):
     template_name = 'login.html'
     form_class = LoginForm
+    success_message = _('You are logged in')
