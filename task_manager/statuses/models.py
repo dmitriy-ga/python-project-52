edf0879397da4c0fe0446a6d8f291de9655d3814
dmitriy-ga <Dmitriy2243@yandex.ru>
2023-05-30 14:38:30 +0500

    Update: translate models fields

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class StatusModel(models.Model):
     # objects = models.Manager()
-    name = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=200, unique=True,
+                            verbose_name=_('name'))
 
     def __str__(self):
         return self.name
--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -2,25 +2,29 @@ from django.db import models
 from task_manager.users.models import User
 from task_manager.statuses.models import StatusModel
 from task_manager.labels.models import LabelModel
+from django.utils.translation import gettext_lazy as _
 
 
 class TaskModel(models.Model):
     objects = models.Manager()
-    name = models.CharField(max_length=200, unique=True)
-    description = models.TextField(max_length=200)
+    name = models.CharField(max_length=200, unique=True,
+                            verbose_name=_('name'))
+    description = models.TextField(max_length=200,
+                                   verbose_name=_('description'))
 
     author = models.ForeignKey(User, on_delete=models.PROTECT,
-                               related_name='author')
+                               related_name='author', verbose_name=_('author'))
 
     executor = models.ForeignKey(User, on_delete=models.PROTECT,
-                                 related_name='executor')
+                                 related_name='executor',
+                                 verbose_name=_('executor'))
 
     status = models.ForeignKey(StatusModel, on_delete=models.PROTECT,
-                               related_name='status')
+                               related_name='status', verbose_name=_('status'))
 
     labels = models.ManyToManyField(LabelModel, through='LabelsThrough',
                                     through_fields=('task', 'label'),
-                                    blank=True)
+                                    blank=True, verbose_name=_('labels'))
 
     def __str__(self):
         return self.name
